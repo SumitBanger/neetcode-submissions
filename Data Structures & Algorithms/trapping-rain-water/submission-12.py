9f61b40aa49be215e4621523ea2052e7bd4354fd
SumitBanger <sumitbanger1995@gmail.com>
2026-05-30 04:00:29 +0530

    Add: trapping-rain-water - submission-12

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-12.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        maxHeightTillNow, maxHeightFromBack = 0, [0] * len(height)
+        for index in range(len(height) - 1, -1, -1):
+            maxHeightFromBack[index] = maxHeightTillNow
+            maxHeightTillNow = max(maxHeightTillNow, height[index])
+
+        print(maxHeightFromBack)
+
+        waterStored, maxHeightTillNow = 0, 0
+        for index, currentHeight in enumerate(height):
+            minHeight = min(maxHeightTillNow, maxHeightFromBack[index])
+            print(f"currentHeight: {currentHeight}, minHeight: {minHeight}")
+            if currentHeight < minHeight:
+                waterStored += (minHeight - currentHeight)
+            maxHeightTillNow = max(maxHeightTillNow, currentHeight)
+
+        
+        return waterStored
+
