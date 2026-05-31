59788b326348a47a921b51a300a73ce7eca6d3bc
SumitBanger <sumitbanger1995@gmail.com>
2026-06-01 01:56:32 +0530

    Add: minimum-window-with-characters - submission-11

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-11.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        countFreq_T, countFreq_S = {}, {}
+        left, right, match, minLenSubstring = 0, 0, 0, ""
+
+        for char in t:
+            countFreq_T[char] = countFreq_T.get(char, 0) + 1
+        
+        while right < len(s): 
+            currentChar = s[right]
+            if currentChar in countFreq_T:
+                countFreq_S[currentChar] = countFreq_S.get(currentChar, 0) + 1
+                if countFreq_S[currentChar] <= countFreq_T[currentChar]:
+                    match += 1
+
+            if match == len(t):
+                leftChar = s[left]
+                while (leftChar not in countFreq_T) or (countFreq_S[leftChar] > countFreq_T[leftChar]):
+                    if leftChar in countFreq_T:
+                        countFreq_S[leftChar] -= 1
+                    left += 1
+                    leftChar = s[left]
+                currentMinLengthStr = s[left:right+1]
+                if minLenSubstring == "" or len(currentMinLengthStr) < len(minLenSubstring):
+                    minLenSubstring = currentMinLengthStr    
+            
+            right += 1
+            
+        return minLenSubstring
