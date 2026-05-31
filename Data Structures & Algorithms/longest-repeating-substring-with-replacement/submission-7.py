1897e7c5ebbaba117884d4f88e6e175f2457d4c9
SumitBanger <sumitbanger1995@gmail.com>
2026-05-31 15:17:42 +0530

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left, maxLength, currentWindowLength, maxCharCount = 0, 0, 0, 0
+        count = {}
+        for index, char in enumerate(s):
+            count[char] = count.get(char,0) + 1
+
+            maxCharCount = max(count.values())  
+            currentWindowLength = index - left + 1
+
+            if currentWindowLength - maxCharCount <= k:
+                print(f"inside if, currentWindowLength: {currentWindowLength}, maxCharCount: {maxCharCount}, index: {index}, left: {left}")
+                maxLength = max(maxLength, currentWindowLength)
+            else:
+                print(f"inside else, currentWindowLength: {currentWindowLength}, maxCharCount: {maxCharCount}, index: {index}, left: {left}")
+                count[s[left]] = count.get(s[left],0) - 1
+                left += 1
+
+        
+        return maxLength
+        
