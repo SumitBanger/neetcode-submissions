f3c0f67f227c8f8cb3394bc88e4067d7c13310d8
SumitBanger <sumitbanger1995@gmail.com>
2026-06-06 00:15:26 +0530

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        openToCloseMap = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+        stack = []
+
+        for char in s:
+            if char not in openToCloseMap:
+                stack.append(char)
+            elif stack and stack[-1] == openToCloseMap[char]:
+                stack.pop()
+            else:
+                return False
+        
+        return True if not stack else False
+
+
+        # for char in s:
+        #     if char in openToCloseMap.values():
+        #         stack.append(char)
+        #     elif stack and stack[-1] == openToCloseMap[char]:
+        #         stack.pop()
+        #     else:
+        #         stack.append(char)
+        #         break
+
+        # return True if not stack else False
+        
+        
