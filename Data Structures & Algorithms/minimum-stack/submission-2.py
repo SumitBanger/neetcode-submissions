171709035b2c36dd79de65cfbf1e40889209e5de
SumitBanger <sumitbanger1995@gmail.com>
2026-06-06 00:25:03 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.py
@@ -0,0 +1,43 @@
+class MinStack:
+    def __init__(self):
+        self.minStack = []
+        self.mainStack = []
+
+    def push(self, val: int) -> None:
+        self.mainStack.append(val)
+        minEle = min(val, self.minStack[-1]) if self.minStack else val
+        self.minStack.append(minEle)
+
+    def pop(self) -> None:
+        self.mainStack.pop()
+        self.minStack.pop()
+            
+    def top(self) -> int:
+        return self.mainStack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+
+
+
+    # def __init__(self):
+    #     self.minStack = []
+    #     self.mainStack = []
+
+    # def push(self, val: int) -> None:
+    #     self.mainStack.append(val)
+    #     minVal = min(val, self.minStack[-1] if self.minStack else val)
+    #     self.minStack.append(minVal)
+
+    # def pop(self) -> None:
+    #     self.minStack.pop()
+    #     self.mainStack.pop()
+            
+    # def top(self) -> int:
+    #     return self.mainStack[-1]
+
+    # def getMin(self) -> int:
+    #     return self.minStack[-1]
+        
