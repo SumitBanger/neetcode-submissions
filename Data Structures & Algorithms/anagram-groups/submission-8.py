a83a32be7d6fb8d1ecd1fb6a5f51dbd49957944c
SumitBanger <sumitbanger1995@gmail.com>
2026-06-07 20:57:07 +0530

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,37 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        countToAnagramsMap = defaultdict(list)
+        for string in strs:
+            count = [0] * 26
+            for char in string:
+                charIndex = ord(char) - ord('a')
+                count[charIndex] += 1
+            countToAnagramsMap[tuple(count)].append(string)
+        
+        return list(countToAnagramsMap.values())
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        frequencyToStringsMap = {}
+        for string in strs:
+            frequencyTuple = self.getFrequencyMapTuple(string)
+            if frequencyTuple not in frequencyToStringsMap:
+                frequencyToStringsMap[frequencyTuple] = []
+            frequencyToStringsMap[frequencyTuple].append(string)
+
+        return list(frequencyToStringsMap.values())
+
+    def getFrequencyMapTuple(self, string):
+        frequency = {}
+        for char in string:
+            frequency[char] = frequency.get(char, 0) + 1
+        return tuple(sorted(frequency.items()))
