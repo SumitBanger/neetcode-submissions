6a127392a3af058de274813e0392d9cc9f515166
SumitBanger <sumitbanger1995@gmail.com>
2026-06-08 19:58:27 +0530

    Add: longest-substring-without-duplicates - submission-11

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-11.py
@@ -0,0 +1,72 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen, longestTillNow = set(), 0
+        left, right = 0, 0
+        while left <= right < len(s):
+            currentChar = s[right]
+            if currentChar not in seen:
+                seen.add(currentChar)
+                right += 1
+            else:
+                longestTillNow = max(longestTillNow, right - left)
+                while s[left] != currentChar:
+                    seen.remove(s[left])
+                    left += 1
+                left += 1
+                right += 1  
+        
+        return max(longestTillNow, right - left)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        seen = set()
+        left, maxLength = 0, 0
+        for char in s:
+            while char in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(char)
+            maxLength = max(maxLength, len(seen))
+        
+        return maxLength
+
+
+        # My Original Idea
+        seen = set()
+        if len(s) <= 1:
+            return len(s)
+        
+        left, right, maxLength = 0, 1, 1
+        seen.add(s[left])
+        while left < right < len(s):
+            currentChar = s[right]
+            if currentChar not in seen:
+                seen.add(currentChar)
+                maxLength = max(maxLength, len(seen))
+            else:
+                while s[left] != currentChar:
+                    seen.remove(s[left])
+                    left += 1
+                maxLength = max(maxLength, len(seen))
+                left += 1
+            right += 1
+                
+
+        return maxLength
