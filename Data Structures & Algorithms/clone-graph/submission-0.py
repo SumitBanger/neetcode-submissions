dda8b5b5e0f3e7aea5fe84518dc84bece550b5f3
SumitBanger <sumitbanger1995@gmail.com>
2026-07-26 04:08:45 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,36 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        nodeToCopyMap, visited, Q, clonedHead = {}, set(), deque([node]), None
+        if not node: return None
+
+        while Q:
+            currentNode = Q.popleft()
+            visited.add(currentNode)
+            copyNode = Node(currentNode.val)
+            nodeToCopyMap[currentNode] = copyNode
+            for neighbor in currentNode.neighbors:
+                if neighbor and neighbor not in visited:
+                    Q.append(neighbor)
+        
+        for originalNode, copyNode in nodeToCopyMap.items():
+            copyNeighbors = []
+            for neighbor in originalNode.neighbors:
+                copyNeighbors.append(nodeToCopyMap[neighbor])
+            copyNode.neighbors = copyNeighbors
+            if not clonedHead:
+                clonedHead = copyNode
+
+        return clonedHead
+
+                
+
+
+        
