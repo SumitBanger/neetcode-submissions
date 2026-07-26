08e0982f2dcdf37ddcbad0f4db3694df67688bce
SumitBanger <sumitbanger1995@gmail.com>
2026-07-27 03:17:13 +0530

    Add: pacific-atlantic-water-flow - submission-3

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-3.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROW, COL, Q, result = len(heights), len(heights[0]), deque(), []
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        store = [[0] * COL for _ in range(ROW)]
+
+        for row in range(ROW):
+            for col in range(COL):
+                if row == 0 or col == 0:
+                    store[row][col] += 1
+                    Q.append((row, col))
+        while Q:
+            for index in range(len(Q)):
+                r, c = Q.popleft()
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    if (row in range(ROW)) and (col in range(COL)) and store[row][col] < 1 and heights[row][col] >= heights[r][c]:
+                        Q.append((row,col))
+                        store[row][col] += 1
+        Q = deque()
+        for row in range(ROW):
+            for col in range(COL):
+                if row == ROW - 1 or col == COL - 1:
+                    store[row][col] += 2
+                    Q.append((row, col))
+        while Q:
+            for index in range(len(Q)):
+                r, c = Q.popleft()
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+                    if (row in range(ROW)) and (col in range(COL)) and store[row][col] < 2 and heights[row][col] >= heights[r][c]:
+                        Q.append((row,col))
+                        store[row][col] += 2
+
+        for row in range(ROW):
+            for col in range(COL):
+                if store[row][col] == 3:
+                    result.append([row, col])
+        return result
