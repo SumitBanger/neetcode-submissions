97f5f0fcd008f8d0f4f83e924297ec0634d8081b
SumitBanger <sumitbanger1995@gmail.com>
2026-07-28 01:42:25 +0530

    Add: count-connected-components - submission-4

---
+++ Data Structures & Algorithms/count-connected-components/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        parent = {}
+        rank = {}
+        for i in range(n):
+            parent[i] = i
+            rank[i] = 1
+        result = n
+
+        def find(node): # Responsible for finding the topmost parent of given node
+            while node != parent[node]:
+                parent[node] = parent[parent[node]]
+                node = parent[node]
+            return node
+        
+        def union(node1, node2): # Responsible for merging both nodes based on Rank
+            par1, par2 = find(node1), find(node2)
+
+            if par1 == par2: # Both the nodes already have common parent so are already connected so no need of union
+                return 0
+
+            if rank[par1] > rank[par2]:
+                parent[par2] = par1
+                rank[par1] += rank[par2]
+            else:
+                parent[par1] = par2
+                rank[par2] += rank[par1]
+            return 1
+
+        for node1, node2 in edges:
+            result -= union(node1, node2)
+
+        return result
+        
