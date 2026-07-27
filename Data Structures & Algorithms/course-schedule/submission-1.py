f0e5272ccf8dc9fe194afad7a5257d2bbd97006f
SumitBanger <sumitbanger1995@gmail.com>
2026-07-27 05:34:33 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        result, visited, completed = False, set(), set()
+        preReqMap = {i:[] for i in range(numCourses)}
+        for [a, b] in prerequisites:
+            preReqMap[a].append(b)
+
+        def dfs(current):
+            if current in visited: # We found a loop so return False
+                return False
+
+            if current in completed or preReqMap[current] == []:
+                completed.add(current)
+                return True
+            
+            visited.add(current)
+            preReq = preReqMap[current]
+            while preReq:
+                if not dfs(preReq[-1]): return False
+                preReq.pop()
+            #preReqMap[current] = []
+            visited.remove(current)
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i): return False
+
+        return True
+        
