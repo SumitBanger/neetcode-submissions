fe75fb3139966671b0c339bc75ecd2caf201fb93
SumitBanger <sumitbanger1995@gmail.com>
2026-08-03 17:39:21 +0530

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''
+        Bounds: N -> (0, n)
+        Order: small before big -> 0 to n
+        '''
+        dp = [1] * (n+1)
+        for i in range(0, n+1):
+            if i == 0 or i == 1:
+                dp[i] = 1
+                continue
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+
+        # DP = [-1] * (n+1)
+        # DP[0], DP[1] = 1, 1 
+
+        # def num_ways(N):
+        #     if DP[N] > 0:
+        #         return DP[N]
+        #     DP[N] = num_ways(N-1) + num_ways(N-2)
+        #     return DP[N]
+        
+        # return num_ways(n)
+        
