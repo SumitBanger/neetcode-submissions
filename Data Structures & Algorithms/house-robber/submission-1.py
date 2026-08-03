581b01f41e07cfa173c056d999573bb5daed4d66
SumitBanger <sumitbanger1995@gmail.com>
2026-08-03 18:19:01 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        total = len(nums)
+        '''
+        Bound: [0, total - 1], dp[total] size
+        Order: small before big -> for 0 to total
+        Base Cases: dp[0] = nums[0] (no other choice), dp[1] = max(nums[0], nums[1])
+        '''
+        if total <= 1:
+            return nums[0]
+        dp = [0] * total
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        for i in range(2, total):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+        return dp[total -1]
+
+
+        
+        
