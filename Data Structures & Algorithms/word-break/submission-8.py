8d1d45c8ed790af0676e0fc1524f53b866c34932
SumitBanger <sumitbanger1995@gmail.com>
2026-08-08 00:38:40 +0530

    Add: word-break - submission-8

---
+++ Data Structures & Algorithms/word-break/submission-8.py
@@ -0,0 +1,36 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        strLen = len(s)
+        # def canBeSegmented(i):
+        #     if(i == strLen):
+        #         return True
+        #     isPossible = False
+        #     for word in wordDict:
+        #         wordLen = len(word)
+        #         if i + wordLen <= strLen and s[i: i+wordLen] == word:
+        #             isPossible |= canBeSegmented(i+wordLen);
+        #             if isPossible: break
+        #     return isPossible
+        '''
+        Bounds: i -> [0, strLen]
+        Order: i
+        '''
+
+        dp = {strLen: True}
+        def canBeSegmented(i):
+            # Base case: reached the end of the string successfully
+            if i in dp: return dp[i] 
+            dp[i] = False
+            # Try every word in the dictionary
+            for word in wordDict:
+                wordLen = len(word)
+                # Check if the word fits and matches the current prefix
+                if i + wordLen <= strLen and s[i : i + wordLen] == word:
+                    # Recursively check the rest of the string
+                    if canBeSegmented(i + wordLen):
+                        return True # Exit immediately ONLY if it works!
+                        #break
+            # If no words in the dictionary lead to a valid segmentation
+            return dp[i]
+            
+        return canBeSegmented(0)
