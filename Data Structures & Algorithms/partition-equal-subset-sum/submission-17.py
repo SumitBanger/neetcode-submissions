9fe5ac6034b387ccbe2fe5741116fc991f325676
SumitBanger <sumitbanger1995@gmail.com>
2026-08-08 05:26:36 +0530

    Add: partition-equal-subset-sum - submission-17

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-17.py
@@ -0,0 +1,44 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total, totalSum = len(nums), sum(nums)
+        if total == 1 or totalSum % 2 == 1: # totalSum is odd Or total count is 1
+            return False
+        targetSum = totalSum // 2
+        dp = [True] + [False]*(targetSum)
+
+        for i in range(0, total):
+            for target in range(targetSum, nums[i] - 1, -1):
+                dp[target] = dp[target] or dp[target - nums[i]]
+        return dp[targetSum]
+        # dp = {}
+
+        '''
+        Bounds: i: [0, total], target: [0, targetSum]
+        Order: 
+            - i: big before small - [total, 0]
+            - target: small before big - [0, targetSum]
+        BaseCase: i == total: False, target == 0: True
+        '''
+        # dp = [[False]*(targetSum+1) for _ in range(total+1)]
+        # for i in range(total):
+        #     dp[i][0] = True
+
+
+
+        # def isPossible(i, target):
+        #     if target == 0:
+        #         dp[(i, target)] = True
+        #     if i == total:
+        #         dp[(i, target)] = False
+            
+        #     if (i, target) in dp: return dp[(i, target)]
+            
+        #     pick = False
+        #     if nums[i] <= target:
+        #         pick = isPossible(i+1, target - nums[i])
+        #     notPick = isPossible(i+1, target)
+        #     dp[(i, target)] = pick or notPick
+        #     return dp[(i, target)]
+        # return isPossible(0, targetSum)
+
+        
