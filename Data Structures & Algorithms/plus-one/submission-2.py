cf435ef0c1d402b55c9be6c939971f540981dce6
SumitBanger <sumitbanger1995@gmail.com>
2026-08-15 03:14:39 +0530

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        output, currSum, carry = [], 0, 1
+        for digit in reversed(digits):
+            carry, currSum = divmod(digit + carry, 10)
+            output.insert(0, currSum)
+        output.insert(0, carry) if carry > 0 else None
+        return output
+
+        
